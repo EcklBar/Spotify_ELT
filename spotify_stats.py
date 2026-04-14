73fb982181eb9a1e6a6e6dce34fd45e59dcdc30d
EcklBar <bar.eckl@teads.com>
2026-04-14 21:24:07 +0300

    Refactor Spotify API integration to retrieve top artists by genre and update function names for clarity

--- spotify_stats.py
+++ spotify_stats.py
@@ -8,13 +8,13 @@ load_dotenv(dotenv_path="./.env")
 
 client_id = os.getenv("SPOTIFY_CLIENT_ID")
 client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
-artist_names = json.loads(os.getenv("ARTIST_NAMES"))
 
 def get_access_token():
     
-    token_url = "https://accounts.spotify.com/api/token"
-    
     try: 
+        
+        token_url = "https://accounts.spotify.com/api/token"
+        
         # Encode credentials
         auth_string = f"{client_id}:{client_secret}"
         auth_bytes = auth_string.encode("utf-8")
@@ -31,59 +31,41 @@ def get_access_token():
             )
 
         response.raise_for_status()
-
-        token = response.json()["access_token"]
+        # Print the response
+        response_json = response.json()
+        # print(json.dumps(response_json, indent=4))
+        
+        token = response_json["access_token"]
         
         return token
     
     except requests.exceptions.RequestException as e:
      		raise e
 
-def get_artist_ids(token, artist_names):
+def get_top_ten_artists_by_genre(token, genre, limit=10):
     
     try:
-         
+        
+        url = f"https://api.spotify.com/v1/search?q=genre:{genre}&type=artist&limit={limit}"
         headers = {"Authorization": f"Bearer {token}"}
-        artist_ids = []
-
-        for name in artist_names:
-            url = f"https://api.spotify.com/v1/search?q={name}&type=artist&limit=1"
-
-            response = requests.get(url, headers=headers)
         
-            response.raise_for_status()
-
-            artist = response.json()["artists"]["items"][0]
-            artist_ids.append(artist["id"])
-
-            print(f"{artist['name']} → {artist['id']}")
-
-        return artist_ids
-        
-    except requests.exceptions.RequestException as e:
-        raise e
-
-def get_artist_album(token, artist_ids):
-    
-    headers = {"Authorization": f"Bearer {token}"}
-    all_albums = []
-    
-    for artist_id in artist_ids: 
-        url = f"https://api.spotify.com/v1/artists/{artist_id}/albums"
-        
+        # A GET request
         response = requests.get(url, headers=headers)
         
         response.raise_for_status()
+
+        response_json = response.json()
         
-        albums = response.json()["items"]
-        all_albums.extend(albums)
+        artists = response_json["artists"]["items"]
         
-        print(json.dumps(albums, indent=4))
+        print(artists[0].keys())
+        
+        return artists
     
-    return all_albums
+    except requests.exceptions.RequestException as e:
+        raise e 
     
 
 if __name__ == "__main__":
     token = get_access_token()
-    artist_ids = get_artist_ids(token, artist_names)
-    print(get_artist_album(token, artist_ids))
+    get_top_ten_artists_by_genre(token, "pop", 10)
