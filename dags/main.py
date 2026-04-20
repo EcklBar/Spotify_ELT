1d902a61b7eeb9ff9ad9263f7e84675cbd18c1ef
EcklBar <bar.eckl@teads.com>
2026-04-20 10:41:56 +0300

    Add new DAG for updating database and enhance main.py with Spotify data processing tasks. Introduced staging and core table updates, and added a new JSON data file for April 20, 2026, containing detailed artist and album information.

--- dags/main.py
+++ dags/main.py
@@ -1,7 +1,19 @@
 from airflow import DAG
 import pendulum
 from datetime import datetime, timedelta
-from api.spotify_stats import get_access_token, get_artist_ids, get_artist_albums, get_album_tracks, save_to_json, artist_names
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
+from api.spotify_stats import (
+    get_access_token,
+    get_artist_ids,
+    get_artist_albums,
+    get_album_tracks,
+    save_to_json,
+    artist_names,
+)
+
+from datawarehouse.dwh import staging_table, core_table
+
 
 # Define the local timezone
 local_tz = pendulum.timezone("Asia/Jerusalem")
@@ -39,3 +51,20 @@ with DAG(
     
     # Define dependencies
     token >> artists >> albums >> tracks >> save_to_json_task
+    
+    
+# DAG 2: update_db
+with DAG(
+    dag_id="update_db",
+    default_args=default_args,
+    description="DAG to process JSON file and insert data into both staging and core schemas",
+    schedule="0 15 * * *",
+    catchup=False,
+) as dag:
+    
+    # Define tasks
+    update_staging = staging_table()
+    update_core = core_table()
+    
+    # Define dependencies
+    update_staging >> update_core
